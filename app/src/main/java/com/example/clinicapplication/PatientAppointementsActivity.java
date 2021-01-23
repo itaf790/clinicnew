333edf2670dc766248453792e02fc58e43ab4868
itaf <itafissam3@gmail.com>
2021-01-23 16:29:51 +0200

    23 new

---
+++ app/src/main/java/com/example/clinicapplication/PatientAppointementsActivity.java
@@ -0,0 +1,14 @@
+package com.example.clinicapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientAppointementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_appointements);
+    }
+}
